2118ff3f8c497e3b982c339151a4b6de7de9ba58
heleomar <heleomar.silva@consultalogo.com.br>
2021-08-23 13:00:30 -0300

    Foi criado OfertaService e adicionamos as ofertas na home.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
+import { OfertasService } from '../ofertas.service';
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from '../shared/oferta.model'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertasService ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Array<Oferta>
 
-  ngOnInit(): void {
+  constructor(private ofertasService: OfertasService) { }
+
+  ngOnInit() {    
+    this.ofertas = this.ofertasService.getOfertas();
+    console.log(this.ofertas)
+    
   }
 
 }
