d5a49535483ce0758e0ddf8bd027be2f4fc86bf5
heleomar <heleomar.silva@consultalogo.com.br>
2021-08-31 15:40:48 -0300

    Ajustando URL da API

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,18 +1,19 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Oferta } from './shared/oferta.model'
+import { URL_API } from './app.api'
 
 
 @Injectable()
 export class OfertasService {
 
-
+   
     constructor(private http: HttpClient) { }
 
 
     public getOfertas(): Promise<Array<Oferta>> {
 
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(`${URL_API}?destaque=true`)
             .toPromise() //convertendo para Promise, pq ele é um observable!
             .then((resposta: any) => resposta)
     }
@@ -30,9 +31,23 @@ export class OfertasService {
     //    });
     //}
 
-    public getOfertasPorCategoria(categoria: String) : Promise<Array<Oferta>> {
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
-        .toPromise()
-        .then()
+    public getOfertasPorCategoria(categoria: String): Promise<Array<Oferta>> {
+        return this.http.get(`${URL_API}?categoria=${categoria}`)
+            .toPromise()
+            .then()
     }
+
+    public getOfertaPorId(id: number): Promise<Oferta> {
+        return this.http.get(`${URL_API}?id=${id}`)
+            .toPromise()
+            .then((resposta: any) => {
+             // return resposta[0] // pega a primeira posição do Array!
+                return resposta.shift() // shift pega a primeira posição do Array!
+            })            
+                             
+           
+        
+    }
+
+
 }
